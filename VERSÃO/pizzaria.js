cc89b3bc086fec0176d969a53676049d20d0260b
Daniel Charlo <danielhenriquecharlo@gmail.com>
2025-05-15 16:57:40 -0300

    Atualização 15-05

--- VERSÃO/pizzaria.js
+++ VERSÃO/pizzaria.js
@@ -1,8 +1,26 @@
-function mostrarConteudo(conteudo) {
-    // Esconde todas as seções
-    document.getElementById("adicionar").classList.add("hidden");
-    document.getElementById("cardapio").classList.add("hidden");
+let pizzaria = [];
 
-    // Mostra a seção correspondente ao botão clicado
-    document.getElementById(conteudo).classList.remove("hidden");
+function mostrarConteudo(conteudo) {
+document.getElementById("adicionar").classList.add("hidden");
+document.getElementById("cardapio").classList.add("hidden");
+
+document.getElementById(conteudo).classList.remove("hidden");
+}
+
+function adicionarPizza() {
+const sabor = document.getElementById("sabor").value;
+const igredientes = document.getElementById("igredientes").value;
+const valor = parseFloat(document.getElementById("valor").value);
+
+if (sabor && igredientes && valor) {
+    pizzaria.push({sabor , igredientes , valor})
+    document.getElementById("sabor").value = ""
+    document.getElementById("igredientes").value = ""
+    document.getElementById("valor").value = ""
+    alert("Pizza adicionada com sucesso!")
+} else {
+    alert("Erro nas informações!")
+}
+
+
 }
