88a8ed1beb14590dd7f4427698ef69da69367fea
Daniel Charlo <danielhenriquecharlo@gmail.com>
2025-05-22 16:57:09 -0300

    atualização

--- VERSÃO/pizzaria.js
+++ VERSÃO/pizzaria.js
@@ -9,18 +9,38 @@ document.getElementById(conteudo).classList.remove("hidden");
 
 function adicionarPizza() {
 const sabor = document.getElementById("sabor").value;
-const igredientes = document.getElementById("igredientes").value;
+const ingredientes = document.getElementById("ingredientes").value;
 const valor = parseFloat(document.getElementById("valor").value);
 
-if (sabor && igredientes && valor) {
-    pizzaria.push({sabor , igredientes , valor})
+if (sabor && ingredientes && valor) {
+    pizzaria.push({sabor , ingredientes , valor})
     document.getElementById("sabor").value = ""
-    document.getElementById("igredientes").value = ""
+    document.getElementById("ingredientes").value = ""
     document.getElementById("valor").value = ""
     alert("Pizza adicionada com sucesso!")
+    atualizarTabela();
 } else {
     alert("Erro nas informações!")
 }
+}
+function pesquisarPizza(){
+    const pesquisa = document.getElementById("pesquisar").value.toLowerCase();
+    const resultados = pizzaria.filter((pizza) => pizza.sabor.toLowerCase().includes(pesquisa));
+    atualizarTabela(resultados);
 
 
+function atualizarTabela(lista = pizzaria) {
+      const tabela = document.getElementById("tabela-pizzas");
+      tabela.innerHTML = "";
+
+      lista.forEach((pizza) => {
+        const linha = document.createElement("tr");
+        linha.innerHTML = `
+          <td>${pizza.sabor}</td>
+          <td>${pizza.ingredientes}</td>
+          <td>${pizza.valor.toFixed(2)}</td>
+        `;
+        tabela.appendChild(linha);
+      });
+    }
 }
